9412dc2e3abc1a63281e356bee710d526a0bdba3
TanHoangNhat-HD <tan.hoang@hdwebsoft.co>
2021-12-02 15:15:11 +0700

    Done fetch TodosList with react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import '../node_modules/antd/dist/antd.css';
 import Todos from 'features/todos/pages/Todos';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <Todos />
-    </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
